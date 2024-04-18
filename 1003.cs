bebb3f457e0d16fe6819cb13476a75c2c1e43f28
Augusto Pereira Aguiar <78914937+AugustoPAguiar@users.noreply.github.com>
2024-04-18 17:04:09 +0100

    Soma Simples

---
+++ 1003.cs
@@ -0,0 +1,18 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int A, B, SOMA;
+        
+        A = Convert.ToInt32(Console.ReadLine());
+        B = Convert.ToInt32(Console.ReadLine());
+        
+        SOMA = A+B;
+        
+        Console.WriteLine("SOMA = " + SOMA);
+
+    }
+
+}
