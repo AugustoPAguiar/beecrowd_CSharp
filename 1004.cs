691b6be8594369d0536b1fe9a1e42ec5cbfeee3f
Augusto Pereira Aguiar <78914937+AugustoPAguiar@users.noreply.github.com>
2024-04-18 17:04:39 +0100

    Produto Simples

---
+++ 1004.cs
@@ -0,0 +1,18 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int X, Y, PROD;
+
+        Y = Convert.ToInt32(Console.ReadLine());
+        X = Convert.ToInt32(Console.ReadLine());
+        
+        PROD = X * Y;
+        
+        Console.WriteLine("PROD = " + PROD);
+
+    }
+
+}
