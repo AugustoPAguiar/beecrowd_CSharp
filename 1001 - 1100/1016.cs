fdeb89a0090c717a412d5a39b930a2e05d9cdd56
Augusto Pereira Aguiar <78914937+AugustoPAguiar@users.noreply.github.com>
2024-04-23 16:59:09 +0100

    Distância

---
+++ 1001 - 1100/1016.cs
@@ -0,0 +1,15 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int km = Convert.ToInt32(Console.ReadLine());
+        
+        int tempo = km*2;
+        
+        Console.WriteLine("{0} minutos", tempo);
+
+    }
+
+}
